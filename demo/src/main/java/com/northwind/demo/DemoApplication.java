12f5ab96ceb855c3c2c0ac7c0061ce28cbc57ad7
aneathesia <2394251482@qq.com>
2021-01-05 16:33:16 +0800

    test commit

--- demo/src/main/java/com/northwind/demo/DemoApplication.java
+++ demo/src/main/java/com/northwind/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//test git add commit  push
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
