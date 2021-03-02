81eaa105daae01d2f909bf4ebfcc92a347051b44
aneathesia <2394251482@qq.com>
2021-03-02 21:41:43 +0800

    add gridmanager

--- vuespring/src/main.js
+++ vuespring/src/main.js
@@ -3,7 +3,9 @@ import "./plugins/axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import $gridManager,{jTool} from 'gridmanager';
+import 'gridmanager/css/gm.css';
+export {$gridManager,jTool};
 Vue.config.productionTip = false;
 
 new Vue({
